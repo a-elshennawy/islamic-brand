b98d728ba63d6d71c20d7173d204999d8921ce73
shennawy <ahmedshennawy997@gmail.com>
2026-02-21 00:20:02 +0200

    useEffect

--- src/components/Loaders/Loader.jsx
+++ src/components/Loaders/Loader.jsx
@@ -1,12 +1,19 @@
 import logo from "../../assets/images/logo.png";
 import "./loaders.css";
+import useMobile from "../../hooks/useMobile";
 
 function Loader() {
+  const { isMobile } = useMobile();
   return (
     <>
       <div className="Loader text-center">
         <div className="imgHolder p-0">
-          <img src={logo} alt="logo" loading="eager" />
+          <img
+            src={logo}
+            alt="logo"
+            loading="eager"
+            style={{ width: isMobile ? "30%" : "10%" }}
+          />
         </div>
       </div>
     </>
