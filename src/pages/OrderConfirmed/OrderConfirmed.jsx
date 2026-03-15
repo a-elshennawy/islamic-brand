c7e422948a60ecf81fe5e48db3de417a87daa332
shennawy <ahmedshennawy997@gmail.com>
2026-03-16 00:23:28 +0200

    order confirmation page

--- src/pages/OrderConfirmed/OrderConfirmed.jsx
+++ src/pages/OrderConfirmed/OrderConfirmed.jsx
@@ -30,10 +30,12 @@ function OrderConfirmed() {
 
   if (isLoading) return <Loader />;
 
+  const orderItems = order?.order_details;
+
   return (
     <>
-      <section className="orderConfirmedPage row justify-content-center align-items-center m-0 gap-2">
-        <div className="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-4 p-0">
+      <section className="orderConfirmedPage row justify-content-center align-items-start m-0 gap-2">
+        <div className="col-xl-1 col-lg-1 col-md-2 col-sm-3 col-3 p-0">
           <Lottie animationData={successLottie} loop={true} />
         </div>
         <div className="col-12 text-center p-0 headerText">
@@ -99,39 +101,52 @@ function OrderConfirmed() {
           </div>
           <div className="tableContent p-2">
             <div className="tableItem p-0">
-              <span>{t("order status")}</span>
-              <span>{order?.status}</span>
-            </div>
-            <hr />
-            <div className="tableItem p-0">
-              <span>{t("payment status")}</span>
-              <span>{order?.payment_status}</span>
-            </div>
-            <hr />
-            <div className="tableItem p-0">
-              <span>{t("shipping fees")}</span>
+              <span>{t("user address")}</span>
               <span>
-                {order?.shipping_cost} {t("L.E")}
+                {order?.user_address?.state?.country?.name} -{" "}
+                {order?.user_address?.state?.name} -{" "}
+                {order?.user_address?.city?.name}
               </span>
             </div>
             <hr />
-            {order?.discount_amount > 0 && (
-              <>
-                <div className="tableItem p-0">
-                  <span>{t("discounts")}</span>
-                  <span>
-                    {order?.discount_amount} {t("L.E")}
-                  </span>
-                </div>
-                <hr />
-              </>
-            )}
             <div className="tableItem p-0">
-              <span>{t("total")}</span>
+              <span>{t("address details")}</span>
               <span>
-                {order?.total} {t("L.E")}
+                {order?.user_address?.address_one} -{" "}
+                {order?.user_address?.address_two} -{" "}
+                {order?.user_address?.district_name}
               </span>
             </div>
+            <hr />
+            <div className="tableItem p-0">
+              <span>{t("shipping method")}</span>
+              <span>
+                {order?.carrier?.name} (+ {order?.carrier?.price} {t("L.E")} )
+              </span>
+            </div>
+          </div>
+        </div>
+
+        {/* order summary*/}
+        <div
+          className="col-xl-5 col-lg-6 col-md-8 col-sm-10 col-10 detailsTable p-0"
+          style={{ direction: isAr ? "rtl" : "ltr" }}
+        >
+          <div className="tableHead text-center py-2">
+            <h4 className="m-0">{t("order items")}</h4>
+          </div>
+          <div className="tableContent p-2">
+            {orderItems?.map((item) => (
+              <div className="tableItem p-0" key={item?.product_id}>
+                <span>{item?.product?.name}</span>
+                <span>
+                  {item?.price} {t("L.E")} * {item?.quantity}
+                </span>
+                <span>
+                  {t("total")} : {item?.total_price} {t("L.E")}
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       </section>
